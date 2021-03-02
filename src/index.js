51eaa7a82bd359ecd371f3ef4e5fc4d2445c8e9d
Anna <annkulakova96@gmail.com>
2021-03-02 18:38:05 +0300

    new classes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import { model } from './model';
-import { templates} from './templates';
 import './main.css';
 
 // Получаем доступ к блоку 
 const $site = document.querySelector('#site'); // элемент DOM 
 
 model.forEach(block => {
-    const toHTML = templates[block.type]
-    if (toHTML) {
-         // Метод, который позволяет вставлять в определённое место html 
-        $site.insertAdjacentHTML('beforeend', toHTML(block));
-        };
-    });
+    $site.insertAdjacentHTML('beforeend', block.toHTML());
+});
--- src/model.js
+++ src/model.js
@@ -1,10 +1,10 @@
 import image from './assets/image2.png';
-import {Block} from './classes/blocks';
+import {TitleBlock, ImageBlock, TextBlock, ColumnsBlock} from './classes/blocks';
 
 export const model = [
-    new Block('title', 'Website builder with native JavaScript', {
+    new TitleBlock('Website builder with native JavaScript', {
         tag: 'h2',
-        styles: {
+        styles: { 
             margin: '1rem',
             background: 'linear-gradient( to right, #D8BFD8, #B0C4DE)',
             'border-radius': '5px; box-shadow:2px 2px 3px 2px #E6E6FA',
@@ -13,10 +13,10 @@ export const model = [
             padding: '1.5rem',
             'text-align': 'center',
             'font-family': 'var(--bs-font-sans-serif)',
-        }
+        },
     }),
 
-    new Block('image', image, {
+    new ImageBlock(image, {
         styles: {
             margin: '4rem 10rem',
             display: 'flex', 
@@ -26,7 +26,7 @@ export const model = [
             height: 'auto',
         }, alt: 'This is image' }),
 
-    new Block('text', 'What do we get?', {
+    new TextBlock('What do we get?', {
         styles: {
         padding: '1.5rem 0',
         'text-align': 'center',
@@ -36,7 +36,8 @@ export const model = [
         },
     }),
     
-    new Block('columns', [
+    new ColumnsBlock(
+        [
         'Learning a new stuff',
         'Application with native JavaScript',
         'Without frameworks and libraries',
