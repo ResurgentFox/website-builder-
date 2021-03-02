ff43581a9ffbb7b7f4dba72033f404bcbd361e1a
Anna <annkulakova96@gmail.com>
2021-03-02 17:06:15 +0300

    some updates

--- src/model.js
+++ src/model.js
@@ -1,26 +1,54 @@
-import image from './assets/mountain2.jpg';
+import image from './assets/image2.png';
+import {Block} from './classes/blocks';
 
 export const model = [
-    {
-        type: 'title', value: 'Website builder with native JavaScript', options: {
-            tag: 'h2', 
-            styles: {
-                margin: '1rem', 
-                background: 'linear-gradient( to right, #D8BFD8, #B0C4DE)',
-                'border-radius': '5px; box-shadow:2px 2px 3px 2px #E6E6FA',
-                'box-shadow': '2px 2px 3px 2px #E6E6FA',
-                color: '#000',
-                padding: '1.5rem',
-                'text-align': 'center',
-            },
-    }},
-    { type: 'text', value: 'some basic text' },
-    {
-        type: 'columns', value: [
-            'first row',
-            'second row',
-            'third row',
-            'fourth row',
-        ]},
-    { type: 'image', value: image},
+    new Block('title', 'Website builder with native JavaScript', {
+        tag: 'h2',
+        styles: {
+            margin: '1rem',
+            background: 'linear-gradient( to right, #D8BFD8, #B0C4DE)',
+            'border-radius': '5px; box-shadow:2px 2px 3px 2px #E6E6FA',
+            'box-shadow': '2px 2px 3px 2px #E6E6FA',
+            color: '#000',
+            padding: '1.5rem',
+            'text-align': 'center',
+            'font-family': 'var(--bs-font-sans-serif)',
+        }
+    }),
+
+    new Block('image', image, {
+        styles: {
+            margin: '4rem 10rem',
+            display: 'flex', 
+            'justify-content': 'center',
+        }, imageStyles: {
+            width: '600px',
+            height: 'auto',
+        }, alt: 'This is image' }),
+
+    new Block('text', 'What do we get?', {
+        styles: {
+        padding: '1.5rem 0',
+        'text-align': 'center',
+        'font-size': '2rem',
+        'font-weight': '500',
+        'font-family': 'var(--bs-font-sans-serif)',
+        },
+    }),
+    
+    new Block('columns', [
+        'Learning a new stuff',
+        'Application with native JavaScript',
+        'Without frameworks and libraries',
+    ], {
+        styles: {
+            margin: '2rem 4rem',
+            padding: '0 5rem',
+            display: 'flex', 
+            'justify-content': 'space-between',
+            'font-size': '24px',
+            'font-weight': '300',
+            'font-family': 'var(--bs-font-sans-serif)',
+        },
+    }),
 ];
